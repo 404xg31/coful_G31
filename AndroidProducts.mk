7c6612f04a3d42cf07dc62476eb053edd0e23ba0
404xg31 <desimemer11@gmail.com>
2026-08-12 19:46:54 +0530

    Update lunch choices for twrp_coful to version 12.1

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,6 +6,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_coful.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_coful-user \
-    twrp_coful-userdebug \
-    twrp_coful-eng
+    twrp_coful-12.1-user \
+    twrp_coful-12.1-userdebug \
+    twrp_coful-12.1-eng
