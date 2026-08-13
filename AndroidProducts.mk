0f2002f1f20190412cc37c0be10686a739dd01ce
404xg31 <desimemer11@gmail.com>
2026-08-13 13:19:47 +0530

    Remove copyright and license comments
    Removed copyright and license comments from AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,6 +1,6 @@
-# Copyright (C) 2026 The Android Open Source Project
-# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
-# SPDX-License-Identifier: Apache-2.0
+# 
+# 
+# 
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_coful.mk
